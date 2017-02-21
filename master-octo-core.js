36885c1105b2be33be2acc879e14db0b3f9d208c
Rogerio Ribeiro da Cruz <rogeriorc@gmail.com>
2017-02-21 13:31:54 -0300

    Version 0.0.12

--- master-octo-core.js
+++ master-octo-core.js
@@ -716,7 +716,7 @@ var TOTVS;
         return TWebChannel;
     }());
     TWebChannel.instance = null;
-    TWebChannel.version = "0.0.11";
+    TWebChannel.version = "0.0.12";
     TWebChannel.BLUETOOTH_FEATURE = 1;
     TWebChannel.NFC_FEATURE = 2;
     TWebChannel.WIFI_FEATURE = 3;
